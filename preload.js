5b4dddd92b2bc69c79b5260f7412913e70077270
Makerxm800 <makerxm800@gmail.com>
2026-05-02 19:08:25 -0400

    Refactor electronAPI to include isElectron flag

--- preload.js
+++ preload.js
@@ -1,12 +1,9 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
+    isElectron: true,
     getSettings: () => ipcRenderer.invoke('get-settings'),
     saveSettings: (settings) => ipcRenderer.invoke('save-settings', settings),
     selectDownloadFolder: () => ipcRenderer.invoke('select-download-folder'),
-    openExternal: (url) => ipcRenderer.invoke('open-external', url),
-    getAppVersion: () => ipcRenderer.invoke('get-app-version'),
-    getPlatform: () => ipcRenderer.invoke('get-platform'),
-    onOpenSettings: (callback) => ipcRenderer.on('open-settings', callback),
-    isElectron: true
+    getAppVersion: () => ipcRenderer.invoke('get-app-version')
 });
